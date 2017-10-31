b4507c28b19f7967ea335cae4c4133f3c87e3878
Ankur Gupta <ankur.gupta.cse0015@gmail.com>
2017-10-31 19:31:48 +0530

    adding home page ui

---
+++ js/leaderboard.js
@@ -0,0 +1,27 @@
+var img;
+var carousel = function (element) {
+	let items = document.getElementsByClassName('item');
+	startSlide(items,1);
+}
+var startAnimation = function () {
+	carousel();
+}
+var startSlide = function (items, count) {
+	img = items[count];
+	setTimeout(function(){
+		hideAllImages(items);			// hide all images
+		img.classList.remove('hide');	//show current image
+		count = count + 1;
+		if(count >= 4) {
+			count = 0;
+		}
+		startSlide (items , count);
+	}, 4000);
+}
+var hideAllImages = function (items) {
+	let img;
+	for(i=0;i<items.length;i++) {
+		img = items[i];
+		img.classList.add('hide');
+	}
+}
